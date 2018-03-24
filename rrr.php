cd1da6a70a806a35bd81d4222c0324c2f80e9948
N3M0 <tsunghsien1012@gmail.com>
2018-03-25 00:06:21 +0800

    this test!

---
+++ rrr.php
@@ -0,0 +1,18 @@
+<?php
+
+//$target = "http://www.webbotsspidersscreenscrapers.com/hello_world.html";
+
+# Include http library
+# Include libraries
+include("LIB_parse.php");
+include("LIB_http.php");
+
+$web_page = http_get($target="http://www.fbi.gov",$referer="");
+$meta_tag_array = parse_array($web_page["FILE"], "<meta", ">");
+
+for($xx=0; $xx<count($meta_tag_array); $xx++){
+	echo $meta_tag_array[$xx]."\n";
+}
+
+
+?>
